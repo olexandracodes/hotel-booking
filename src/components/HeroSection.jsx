cddc8e01124ddd90115fda3cdf0b6688204ea9c3
Alexandra <121388500+olexandracodes@users.noreply.github.com>
2025-01-03 04:35:59 +0200

    EXPLORE

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography,Button } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
 const HeroSection = () => {
@@ -8,7 +8,7 @@ const HeroSection = () => {
 	return (
 		<Box
 			sx={{
-				height: "640px",
+				height: "560px",
 				width: "100%",
 				backgroundImage:
 					"url(https://images.pexels.com/photos/3225531/pexels-photo-3225531.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)",
@@ -25,9 +25,26 @@ const HeroSection = () => {
 			<Typography variant="h2" sx={{ fontFamily: "'Ubuntu', sans-serif", fontWeight: "bold", fontSize: "80px", marginBottom: "16px", }}>
 				{t("welcome")}
 			</Typography>
-			<Typography sx={{ fontFamily: "'Ubuntu', sans-serif",  fontSize: "24px" }}>
+			<Typography sx={{ fontFamily: "'Ubuntu', sans-serif",  fontSize: "24px", mb: 2, }}>
 				{t("slogan")}
 			</Typography>
+			<Button
+				variant="contained"
+				size="large"
+				sx={{
+					height: "76px",
+					width: "370px",
+					borderRadius: "10px",
+					backgroundColor: "#DFAA5B",
+					color: "#fff",
+					textTransform: "none",
+					padding: "0 16px",
+					fontFamily: "'Ubuntu', sans-serif",
+					fontSize: "28px",
+				}}
+			>
+				{t("explore")}
+			</Button>
 		</Box>
 	);
 };
